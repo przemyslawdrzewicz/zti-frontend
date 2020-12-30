aa97458fb494ede9c66c6079adc12fbc021e1925
Przemyslaw <przemyslaw.drzewicz@o2.pl>
2020-12-30 15:03:56 +0100

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const { prototype } = require('core-js/fn/weak-map')
-const express = require('express')
-const port = process.env.PORT || 8080
-const app = express()
+// const { prototype } = require('core-js/fn/weak-map')
+// const express = require('express')
+// const port = process.env.PORT || 8080
+// const app = express()
 
-app.use(express.static(__dirname + "/dist"))
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html")
+// app.use(express.static(__dirname + "/dist"))
+// app.get(/.*/, function(req, res) {
+//     res.sendfile(__dirname + "/dist/index.html")
+// })
+
+// app.listen(port)
+// console.log("Server stareted...")
+
+var express = require('express')
+var history = require('connect-history-api-fallback')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+
+var app = express()
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
+
+// Serve static assets from the build files (images, etc)
+app.use(serveStatic(path.join(__dirname, '/dist')))
+
+var port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log('Server started at http://localhost:5000')
 })
-
-app.listen(port)
-console.log("Server stareted...")
